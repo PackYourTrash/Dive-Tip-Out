cc784f740c71c9996e0e7bd472c9bde2c733cd1a
David Pamatz <davidpamatz@gmail.com>
2026-06-09 13:12:59 -0700

    Initial Xcode project

---
+++ Dive_Tip_OutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dive_Tip_OutApp.swift
+//  Dive Tip-Out
+//
+//  Created by david pamatz on 6/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Dive_Tip_OutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
