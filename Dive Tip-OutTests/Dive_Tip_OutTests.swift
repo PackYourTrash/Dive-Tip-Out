a47bb424c5a606527b0dbd61ce574ccad93f45c5
David Pamatz <davidpamatz@gmail.com>
2026-06-09 13:21:14 -0700

    Add full Xcode project structure

---
+++ Dive Tip-OutTests/Dive_Tip_OutTests.swift
@@ -0,0 +1,17 @@
+//
+//  Dive_Tip_OutTests.swift
+//  Dive Tip-OutTests
+//
+//  Created by david pamatz on 6/9/26.
+//
+
+import Testing
+@testable import Dive_Tip_Out
+
+struct Dive_Tip_OutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
